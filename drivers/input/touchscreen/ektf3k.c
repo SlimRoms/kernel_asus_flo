415831683fd58aacaff11229ebc3478eb8e0db30
Dennis Rassmann <showp1984@gmail.com>
2013-12-07 09:14:15 +0100

    drivers/input/touchscreen/ektf3k: we don't need to define a pointer for a ONE TIME USE! Clean up.
    Change-Id: Ia033bfac72df7899d5570328a8af247caccb030b
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    Signed-off-by: RobbieL811 <robbielancaster811@gmail.com>

--- drivers/input/touchscreen/ektf3k.c
+++ drivers/input/touchscreen/ektf3k.c
@@ -551,7 +551,6 @@ static int elan_ktf3k_ts_get_data(struct i2c_client *client, uint8_t *cmd,
 
 static int elan_ktf3k_ts_read_command(struct i2c_client *client,
 			   u8* cmd, u16 cmd_length, u8 *value, u16 value_length){
-	struct i2c_adapter *adapter = client->adapter;
 	struct i2c_msg msg[1];
 	//__le16 le_addr;
 	int retry = 0;
@@ -563,7 +562,7 @@ static int elan_ktf3k_ts_read_command(struct i2c_client *client,
 
 	for (retry = 0; retry <= ELAN_I2C_RETRY; retry++) {
 		down(&pSem);
-		if (i2c_transfer(adapter, msg, 1) == 1) {
+		if (i2c_transfer(client->adapter, msg, 1) == 1) {
 			up(&pSem);
 			return value_length;
 		}
@@ -578,7 +577,6 @@ static int elan_ktf3k_ts_read_command(struct i2c_client *client,
 
 static int elan_ktf3k_i2c_read_packet(struct i2c_client *client, 
 	u8 *value, u16 value_length){
-	struct i2c_adapter *adapter = client->adapter;
 	struct i2c_msg msg[1];
 	//__le16 le_addr;
 	int retry = 0;
@@ -590,7 +588,7 @@ static int elan_ktf3k_i2c_read_packet(struct i2c_client *client,
 
 	for (retry = 0; retry <= ELAN_I2C_RETRY; retry++) {
 		down(&pSem);
-		if (i2c_transfer(adapter, msg, 1) == 1) {
+		if (i2c_transfer(client->adapter, msg, 1) == 1) {
 			up(&pSem);
 			return value_length;
 		}
